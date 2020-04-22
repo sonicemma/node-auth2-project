6e1a45fb5ef414a69403ab8297d3e11769cd37b2
emma <sonicjane2020@gmail.com>
2020-04-21 23:59:21 -0700

    mvp

--- api/server.js
+++ api/server.js
@@ -1,21 +1,19 @@
 const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
 
+const usersRouter = require('../users/users-router.js');
 const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router');
+const authenticator = require('../auth/authenticator.js');
+
 
 const server = express();
 
-server.use(helmet());
 server.use(express.json());
-server.use(cors());
 
+server.use('/api/users', authenticator, usersRouter);
 server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
-  res.send("It's alive!");
+    res.json({message: 'works'});
 });
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,40 +1,56 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model');
+const secrets = require('../api/secrets.js');
 
-// for endpoints beginning with /api/auth
+function generateToken(user) {
+  const payload = {
+      userId: user.id,
+      username: user.username,
+  };
+  const secret = secrets.jwtSecret;
+  const options = {
+      expiresIn: '1d',
+  };
+  return jwt.sign(payload, secret, options);
+}
+
 router.post('/register', (req, res) => {
   let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+
+  const rounds = process.env.HASH_ROUNDS || 8;
+
+  const hash = bcrypt.hashSync(user.password, rounds);
+
   user.password = hash;
 
   Users.add(user)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+      .then(saved => {
+          res.status(201).json(saved);
+      })
+      .catch(error => {
+          res.status(500).json({ message: error.message});
+      });
 });
 
 router.post('/login', (req, res) => {
   let { username, password } = req.body;
 
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({
-          message: `Welcome ${user.username}!`,
-        });
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+  Users.findBy({username})
+      .then(([user]) => {
+          if (user && bcrypt.compareSync(password, user.password)) {
+              const token = generateToken(user);
+
+              res.status(200).json({message: 'works', token});
+          } else {
+              res.status(401).json({message: 'wrong'});
+          }
+      })
+      .catch(err => {
+          res.status(500).json({message: err.message});
+      });
 });
 
 module.exports = router;
